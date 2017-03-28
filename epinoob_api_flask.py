4f0ba0bc3f6422fa86f090b8530696cb3c1c88ba
Paul THEIS <paultevatheis@gmail.com>
2017-03-28 04:13:42 +0200

    modified:   epinoob_api_flask.py

--- epinoob_api_flask.py
+++ epinoob_api_flask.py
@@ -13,8 +13,37 @@ import logging
 app = Flask(__name__)
 logging.basicConfig(filename=".api.log", level=logging.INFO)
 
+
+def track_event(category, action, label=None, value=0):
+    data = {
+        'v': '1',  # API Version.
+        'tid': 'UA-96333689-1',  # Tracking ID / Property ID.
+        # Anonymous Client Identifier. Ideally, this should be a UUID that
+        # is associated with particular user, device, or browser instance.
+        'cid': '555',
+        't': 'event',  # Event hit type.
+        'ec': category,  # Event category.
+        'ea': action,  # Event action.
+        'el': label,  # Event label.
+        'ev': value,  # Event value, must be an integer
+    }
+
+    response = requests.post(
+        'http://www.google-analytics.com/collect', data=data)
+
+    # If the request fails, this will raise a RequestException. Depending
+    # on your application's needs, this may be a non-error and can be caught
+    # by the caller.
+    response.raise_for_status()
+
+
+
 @app.route('/', methods=['POST', 'GET'])
 def doc():
+    track_event(
+        category='EpiNoob-API',
+        action='Home')
+
     return render_template("epinoob_api_doc.html")
 
 @app.route('/login', methods=['POST', 'GET'])
